4665e0382a01f9d57332a2aaf9573d217e8255a0
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-03-11 23:13:12 -0500

    Add files via upload

---
+++ Number_Eight.py
@@ -0,0 +1,4 @@
+print(2+6) # prints the result of the addition of two integers
+print(16/2) # prints the result of the division of one integer by another
+print(2*4) # prints the result of two integers multiplied by each other
+print(20-12) # prints the result of one integer subtracted from another
---
+++ favorite_number.py
@@ -0,0 +1,3 @@
+favorite_number = 89 # Creates a variable and assigns it an integer
+message = "My absolute favorite number is " + str(favorite_number) + "!" # creates a variable and assigns it a string, which integrates the favorite_number variable converted to a string
+print(message) # prints the variable message
