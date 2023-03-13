02e5a012d73aba9e3856baa816951acafaae0928
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-03-12 23:31:26 -0400

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,5 @@
+friends = ['Matt', 'Mike', 'Kenny']
+
+print(f"Hello {friends[0]}, let's get a beer!")
+print(f"Hey, how have you been {friends[1]}?")
+print(f"{friends[-1]}, I saw that you got a new car!")
---
+++ your_own_list.py
@@ -0,0 +1,6 @@
+motorcycles = ['KTM', 'Honda', 'Yamaha', 'Ducati']
+
+print(f"One of my favorite motorcycle brands is {motorcycles[2]}, they always make quality machines!")
+print(f"On par with {motorcycles[2]} is {motorcycles[1]}, both brands consistently win championships.")
+print(f"My second favorite has to be {motorcycles[-1]} as they have such character and class!")
+print(f"However, better than {motorcycles[1]}, {motorcycles[2]}, or {motorcycles[-1]} is {motorcycles[0]}. Not only do they perform and look amazing, they have the character of a beast!")
