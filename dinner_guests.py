7b61e9767b0d908058b5e0654f0d205f10a9d5a0
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-03-13 23:26:39 -0400

    Add files via upload

---
+++ dinner_guests.py
@@ -0,0 +1,3 @@
+guest_list = ['Dave Weckl', 'Chuck Shuldiner', 'Neil Peart']
+
+print(f"There are {len(guest_list)} guests attending the dinner party.")  
---
+++ seeing_the_world.py
@@ -0,0 +1,27 @@
+want_to_visit = ['Poland', 'Scotland', 'Ireland', 'Japan', 'Norway']
+
+print(want_to_visit)
+
+print(sorted(want_to_visit))
+
+print(want_to_visit)
+
+print(sorted(want_to_visit, reverse=True))
+
+print(want_to_visit)
+
+want_to_visit.reverse()
+
+print(want_to_visit)
+
+want_to_visit.reverse()
+
+print(want_to_visit)
+
+want_to_visit.sort()
+
+print(want_to_visit)
+
+want_to_visit.sort(reverse=True)
+
+print(want_to_visit)
