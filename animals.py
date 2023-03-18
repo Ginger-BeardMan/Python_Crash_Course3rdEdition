c77f90d264c3151b4efb877497f62750f6ae9c1b
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-03-17 21:36:19 -0400

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,7 @@
+canines = ['Wolf', 'Dog', 'Fox']
+
+for canine in canines:
+	print(canine)
+	print(f"A {canine}, may or may not be a good pet, however, they are loyal to their pack")
+
+print("Each of these animals are canines. They have a great sense of smell, are loyal, and can be great hunters!")
--- pizzas.py
+++ pizzas.py
@@ -1,6 +1,7 @@
 pizzas = ['Cheese', 'Pepperoni & Sausage', 'Hawaiian']
 
 for pizza in pizzas:
+	print(pizza)
 	print(f"I like {pizza} pizza! It is yummy!")
 
 print(f"My first favorite pizza is {pizzas[0]}, it is just simply delicious! My next favorite pizza is {pizzas[1]}, it has a little kick! My other favorite pizza is {pizzas[2]}, it has a tropical twist! I love pizzas!")
