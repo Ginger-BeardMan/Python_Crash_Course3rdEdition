9342d88d577b0b28439aa5ce2ad6a549df470957
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-03-20 23:23:07 -0400

    Add files via upload

---
+++ one_million.py
@@ -0,0 +1,4 @@
+# Make a list of the numbers from one to one million, and then use a for loop to print the numbers.
+millions = list(range(1, 1000001))
+for mill in millions:
+	print(mill)
---
+++ threes.py
@@ -0,0 +1,4 @@
+# Make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list.
+
+threes = [value*3 for value in range(1, 11)]
+print(threes)
