1b2195202a648c89841d2357c07fe819f55f20d8
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-03-22 11:33:47 -0400

    Add files via upload

---
+++ conditional_tests.py
@@ -0,0 +1,38 @@
+# Write a series of conditional statements. Print a statement describing each test and your prediction for the results of each test.
+
+best_pizza = 'Pepperoni'
+print('Is best_pizza == Pepperoni? I predict True')
+print(best_pizza == 'Pepperoni')
+
+print('Is best_pizza == Anchovi? I predict False')
+print(best_pizza == 'Anchovi')
+
+coolest_toy = 'Lego'
+print('Is the coolest toy == Dolls? I predict False')
+print(coolest_toy == 'Dolls')
+
+print('Is the coolest toy == bicycle? I predict this is also False')
+print(coolest_toy == 'Bicycle')
+
+print('Is the coolest toy == Lego? I predict True')
+print(coolest_toy == 'Lego')
+
+tool = 'Chisel'
+print('Is the tool I need == chisel? I predict True')
+print(tool == 'Chisel')
+
+print('Is the tool I need == screwdriver? I predict False')
+print(tool == 'Screwdriver')
+
+age = 33
+print('Is my age == 20? I predict False')
+print(age == 20)
+
+print('Is my age == 15? I predict False')
+print(age == 15)
+
+print('Is my age == 33? I predict True')
+print(age == 33)
+
+print('Is my age == 16 + 17? I predict True')
+print(age == 16 + 17)
