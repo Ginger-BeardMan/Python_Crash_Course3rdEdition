41fbbf0d2b74689c7606c346b27eb97ae890b44b
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-03-26 23:00:22 -0400

    Add files via upload

--- favorite_number.py
+++ favorite_number.py
@@ -1,3 +1,15 @@
-favorite_number = 89 # Creates a variable and assigns it an integer
-message = "My absolute favorite number is " + str(favorite_number) + "!" # creates a variable and assigns it a string, which integrates the favorite_number variable converted to a string
-print(message) # prints the variable message
+# Create a dictionary of people's favorite numbers
+
+favorite_numbers = {'Joe': 5, 'Mike': 25, 'Matt': 69, 'Liz': 7, 'Fen': 89}
+
+print(favorite_numbers)
+
+print(f"Joe's favorite number is {favorite_numbers['Joe']}")
+
+print(f"Mike's favorite number is {favorite_numbers['Mike']}")
+
+print(f"Matt's favorite number is {favorite_numbers['Matt']}")
+
+print(f"Liz's favorite number is {favorite_numbers['Liz']}")
+
+print(f"Fen's favorite number is {favorite_numbers['Fen']}")
---
+++ person.py
@@ -0,0 +1,8 @@
+# Use a dictionary to store information about a person you know. 
+
+person = {'first_name': 'John', 'last_name': 'Smith', 'age': 45, 'city': 'San Jose'}
+
+print(person['first_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
