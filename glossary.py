2cc906da54500597ce1548bc3390dea3c24a79af
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-03-28 23:18:31 -0400

    Add files via upload

---
+++ glossary.py
@@ -0,0 +1,11 @@
+# Use five key words that you've learned
+
+key_words = {'string': 'arrays of bytes representing unicode characters', 
+	'variable': 'containers for storing data values', 
+	'class': 'an object constructor', 
+	'integer': 'a whole number without decimals of unlimited length', 
+	'loop': 'used for iterating over a sequence'}
+
+# Print each word and it's meaning as neatly formatted output. 
+
+print(f"string: {key_words['string']}\nvariable: {key_words['variable']}\nclass: {key_words['class']}\ninteger: {key_words['integer']}\nloop: {key_words['loop']}")
