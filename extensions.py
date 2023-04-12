57feae57e12d51bb2583a8c02290deafa395f3bd
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-04-11 23:49:03 -0400

    Add files via upload

---
+++ extensions.py
@@ -0,0 +1,14 @@
+# improve/add to/change context many_users.py
+
+users = {
+	'aeinstein': {
+	'first': 'albert',
+	'last': 'einstein',
+	'location': 'princeton',
+	}
+	'mcurie': {
+	'first': 'marie',
+	'last': 'curie',
+	'location': 'paris',
+	}
+}
