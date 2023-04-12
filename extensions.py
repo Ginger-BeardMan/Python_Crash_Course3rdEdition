f0ba976345dda1954b09662dc2a2213b9453e32e
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-04-12 00:34:50 -0400

    Add files via upload

--- extensions.py
+++ extensions.py
@@ -2,13 +2,26 @@
 
 users = {
 	'aeinstein': {
-	'first': 'albert',
-	'last': 'einstein',
-	'location': 'princeton',
-	}
+		'first': 'albert',
+		'last': 'einstein',
+		'location': 'princeton',
+		},
 	'mcurie': {
-	'first': 'marie',
-	'last': 'curie',
-	'location': 'paris',
+		'first': 'marie',
+		'last': 'curie',
+		'location': 'paris',
+		},
 	}
-}
+
+users['dtownsend'] = {'first': 'Devin', 'last': 'townsend', 'location': 'united states'}
+
+users['todinson'] = {'first': 'thor', 'last': 'odinson', 'location': 'asgard'}
+
+for username, user_info in users.items():
+	print(f"\nUsername: {username}")
+	print(f"User's full name is {user_info['first'].title()} {user_info['last'].title()} and they are located in {user_info['location'].title()}")
+	#full_name = f"{user_info['first']} {user_info['last']}"
+	#location = user_info['location']
+
+	#print(f"\tFull name: {full_name.title()}")
+	#print(f"\tLocation: {location.title()}")
