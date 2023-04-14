8717d695a686df2e3969a552024d10a94c32fc7c
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-04-13 23:57:21 -0400

    Add files via upload

---
+++ rental_car.py
@@ -0,0 +1,9 @@
+rental_car = input('What type of vehicle would you like to rent today?: ')
+
+brand = input('What brand of car is preferred?: ')
+
+model = input('Lastly, what model from that brand is preferred?: ')
+
+print(rental_car)
+print(brand)
+print(model)
