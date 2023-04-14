59319e604089978f00024ba7b8eff1ffa0b2ee2d
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-04-14 00:08:24 -0400

    Add files via upload

--- rental_car.py
+++ rental_car.py
@@ -1,9 +1,15 @@
-rental_car = input('What type of vehicle would you like to rent today?: ')
+car_type = input('What type of vehicle would you like to rent today?: ')
 
-brand = input('What brand of car is preferred?: ')
+brand = input("What brand of car is preferred?(if none, indicate 'no preference'): ")
 
-model = input('Lastly, what model from that brand is preferred?: ')
+if brand != 'no preference':
+	model = input("Lastly, what model from that brand is preferred? (if none, indicate 'no preference'): ")
 
-print(rental_car)
-print(brand)
-print(model)
+print('Great, thank you for that information')
+
+if brand != 'no preference' and model != 'no preference':
+	print(f'Let me take a look to see if we have a {car_type} that is a {brand.title()} {model.title()}')
+elif brand != 'no preference' and model == 'no preference':
+	print(f'Let me take a look to see if we have a {brand.title()} {car_type}')
+elif brand == 'no preference':
+	print(f'Let me check if we have any vehicle that is a {car_type}')
