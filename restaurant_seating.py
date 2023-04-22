7910079f4dd600eb4d0d87c05f7d590563cba223
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-04-21 21:05:50 -0400

    Add files via upload

--- restaurant_seating.py
+++ restaurant_seating.py
@@ -3,7 +3,7 @@ dinner_party = input("How many guests will be attending the dinner party this ev
 guest_count = int(dinner_party)
 
 if guest_count > 8:
-	print('Unfortunately, at this time there is a wait for that number of members.')
-	input('May I take your name?')
+	print('Unfortunately, at this time there is a wait for that number of members. ')
+	input('May I take your name? ')
 else:
-	print('We are able to seat you now. Please follow me.')
+	print('We are able to seat you now. Please follow me. ')
