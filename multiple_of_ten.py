939217dc39a7126cd1ac2d7d8d82f1c888a0d85d
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-04-22 23:09:10 -0400

    Add files via upload

---
+++ multiple_of_ten.py
@@ -0,0 +1,8 @@
+raw_num = input('Tell me a number, any number. ')
+
+final_num = int(raw_num)
+
+if final_num % 10 == 0:
+	print('What a wonderful number, divisible by 10')
+else:
+	print('Thank you for telling me that number.')
