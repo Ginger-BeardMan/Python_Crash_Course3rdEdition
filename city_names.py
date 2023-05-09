f66f65d4e6e719ef1b2ea28ba57ec53447407195
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-05-08 22:00:57 -0400

    Add files via upload

---
+++ city_names.py
@@ -0,0 +1,10 @@
+def city_country(city, country):
+	location = f'{city}, {country}'
+	print(location.title())
+
+
+city_country('oslo', 'norway')
+
+city_country('tokyo', 'japan')
+
+city_country('warsaw', 'poland')
