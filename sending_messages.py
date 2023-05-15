d96d4c4d259031f176f9ba45f0556c30c680e788
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-05-14 23:46:26 -0400

    Add files via upload

--- sending_messages.py
+++ sending_messages.py
@@ -1,16 +1,12 @@
 texts = ['Hello there', 'today is Thursday', 'L is for love']
 sent_messages = []
 
-def show_messages(messages):
-	for message in messages:
-		print(message.title())
-
 def send_messages(messages):
-	for message in messages:
-		print(message.title())
-		sent_message = messages.pop(message)
-		sent_messages.append(message)
-	print(sent_messages)
+	while messages:
+		sent_message = messages.pop()
+		print(f"The current message is {sent_message.title()}")
+		sent_messages.append(sent_message)
 
 send_messages(texts)
+print(sent_messages)
 print(texts)
