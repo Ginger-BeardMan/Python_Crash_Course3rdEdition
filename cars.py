22ad68c370415836a08d1d24c6e9e6cac4b5f75d
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-07-22 22:53:45 -0400

    Add files via upload

--- cars.py
+++ cars.py
@@ -3,6 +3,6 @@ def car_info(manufacturer, model, **car_make):
 	car_make['model'] = model
 	return car_make
 
-car_profile = car_info('Toyota', 'Tacoma', color = 'Orange', tow_package = True)
+car_profile = car_info('Toyota', 'Tacoma', color='Orange', tow_package=True)
 
 print(f'car = {car_profile}') 
---
+++ imports.py
@@ -0,0 +1,9 @@
+import sandwiches
+
+from sandwiches import sandwich
+
+from sandwiches import sandwich as sw
+
+import sandwiches as sws
+
+from sandwiches import *
