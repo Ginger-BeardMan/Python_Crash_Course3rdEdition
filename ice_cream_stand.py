cbb0b7037e3323ee56598fd90e66658252da22e4
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-07-26 00:02:46 -0400

    Add files via upload

--- ice_cream_stand.py
+++ ice_cream_stand.py
@@ -15,6 +15,14 @@ class IceCreamStand(Restaurant):
 
 	def __init__(self, restaurant_name, cuisine_type):
 		super().__init__(restaurant_name, cuisine_type)
+		self.flavors = ['vanilla', 'chocolate', 'strawberry']
+
+	def get_flavors(self):
+		print(f'They currently serve {self.flavors[0], self.flavors[1], self.flavors[2]}')
+
 
 best_stand = IceCreamStand('King Kone', 'Ice Cream')
-print(best_stand.describe_restaurant())
+
+best_stand.describe_restaurant()
+best_stand.open_restaurant()
+best_stand.get_flavors()
