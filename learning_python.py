abac6ab390593e7afc7a28b8913c6d0fc2d78cce
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-08-07 22:00:35 -0400

    Add files via upload

--- learning_python.py
+++ learning_python.py
@@ -1,5 +1,16 @@
 from pathlib import Path
 
-path = Path(learning_python.txt)
+path = Path('learning_python.txt')
+
 contents = path.read_text().rstrip()
+
 print(contents)
+
+learned_items = []
+
+lines = contents.splitlines()
+
+for line in lines:
+	learned_items.append(line)
+	#print(line)
+	print(learned_items[-1])
