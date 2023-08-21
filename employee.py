04663a802cb9150766bb4b26f6eb689a7dc4ebc9
Stephen Karney <112995644+Ginger-BeardMan@users.noreply.github.com>
2023-08-20 23:48:45 -0400

    Add files via upload

--- employee.py
+++ employee.py
@@ -1,7 +1,5 @@
 class Employee():
 
-	attributes = []
-
 	def __init__(self, first_name, last_name, annual_salary):
 		'''take in the name and salary of a worker'''
 		self.first_name = first_name
@@ -12,11 +10,15 @@ class Employee():
 		'''add 5k to salary by default, also allow a random input for a raise'''
 		raise_total = input('How much of a raise will you get this year? 5k or different amount? ')
 		if raise_total == '5k':
-			annual_salary += 5000
-			return annual_salary
+			self.annual_salary += 5000
+			print(f'Your new salary is {self.annual_salary}')
 		else:
-			new_raise = input(int('What is the new total raise? '))
-			annual_salary += new_raise
-			return annual_salary
+			new_raise = input('What is the new total raise? ')
+			self.annual_salary += int(new_raise)
+			print(f'Your new salary is {self.annual_salary}')
 
+employee_john = Employee('John', 'Smith', 100000)
 
+print(f'Hello {employee_john.first_name} {employee_john.last_name}, your current salary is {employee_john.annual_salary}. Is a raise in order?')
+
+employee_john.give_raise()
